50a18fc1d96b9f96456609a9749a3e4483cf7e32
Abhishek Paul <abhishek.paul@mail.utoronto.ca>
2026-05-22 15:52:31 -0400

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.py
@@ -0,0 +1,44 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # dp=[[0]*(n+1) for _ in range(m+1)]
+
+        # dp[m-1][n-1]=1
+        prevRow=[1]*n
+        for r in range(m-1):
+            newRow=[1]*n
+            for c in range(n-2,-1,-1):
+                newRow[c]=newRow[c+1]+prevRow[c]
+            prevRow=newRow
+        
+        return prevRow[0]
+
+
+        # dp=[[0]*(n+1) for _ in range(m+1)]
+
+        # dp[m-1][n-1]=1
+
+        # for r in range(m-1,-1,-1):
+        #     for c in range(n-1,-1,-1):
+        #         dp[r][c]+=dp[r+1][c]+dp[r][c+1]
+        
+        # return dp[0][0]
+
+        
+        # memo={}
+        # ROWS,COLS=m,n
+        # def dp(r,c):
+        #     if (r,c) in memo:
+        #         return memo[(r,c)]
+
+        #     if r<0 or c<0 or r>=m or c>=COLS:
+        #         return 0
+
+        #     if r==m-1 and c==n-1:
+        #         return 1
+
+
+
+        #     memo[(r,c)]=dp(r+1,c)+dp(r,c+1)
+        #     return memo[(r,c)]
+        
+        # return dp(0,0)
